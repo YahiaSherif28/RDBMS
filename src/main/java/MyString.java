affcd528b7ca28ddcca702e5db40e1cb3d196d57
mohammed shetaya <mohammedshetaya22@gmail.com>
2021-06-02 19:12:46 +0200

    string hash

--- src/main/java/MyString.java
+++ src/main/java/MyString.java
@@ -17,4 +17,21 @@ public class MyString implements Comparable<MyString>, Serializable {
     public String toString() {
         return s;
     }
+
+    long p = 257;
+    long mod = 1000000000000037l ;
+
+    public long hashValue (int index){
+        long ret =0;
+        for(int i = index ; i<index+5; i++){
+            int val = i<s.length() ?(int)s.charAt(i) :0;
+
+            ret *= p;
+            ret%=mod;
+            ret+=val;
+            ret%=mod;
+
+        }
+        return ret;
+    }
 }
